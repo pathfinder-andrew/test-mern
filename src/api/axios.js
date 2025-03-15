2350572cd31924b3e29d3de413f6271b355120ce
andrew <>
2025-03-16 03:04:10 +0900

    프로덕션 주소 재수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axiosDefault from "axios";
 const isProduction = process.env.NODE_ENV === "production";
 
 const baseURL = isProduction
-  ? "https://test-mern-xi.vercel.app/api"
+  ? "https://test-mern-xi.vercel.app"
   : "http://localhost:5000";
 
 const defaultOptions = {
