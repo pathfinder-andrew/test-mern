8fe80a6b7953dcfa312749d4758790682dcb887f
andrew <>
2025-03-20 21:54:43 +0900

    서버 호스트 변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axiosDefault from "axios";
 const isProduction = process.env.NODE_ENV === "production";
 
 const baseURL = isProduction
-  ? "https://test-mern-server.onrender.com/"
+  ? "https://test-bridge-mern.onrender.com"
   : "http://localhost:5000";
 
 const defaultOptions = {
